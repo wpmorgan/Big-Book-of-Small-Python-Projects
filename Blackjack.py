d1ae390061c2ff8a43637acc620deaf138aa9d71
Will <wpmorgan@gmail.com>
2023-01-12 19:45:20 +0000

    Blackjack game

--- Blackjack.py
+++ Blackjack.py
@@ -72,4 +72,59 @@ def main():
 
             if move in ('H', 'D'):
                 # Hit/doubling down takes another card.
-                newCard = 
+                newCard = deck.pop()
+                rank,suit = newCard
+                print('You drew a {} of {}'.format(rank, suit))
+                playerHand.append(newCard)
+
+                if getHandValue(playerHand) > 21:
+                    # The player has busted.
+                    continue
+
+            if move in ('S', 'D'):
+                # Stand / doubling down stops the player's turn.
+                break
+
+        # Handle the dealer's actions:
+        if getHandValue(playerHand) <= 21:
+            while getHandValue(dealerHand) < 17:
+                # The dealer hits:
+                print('Dealer hits...')
+                dealerHand.append(deck.pop())
+                displayHands(playerHand, dealerHand, False)
+
+                if getHandValue(dealerHand) > 21:
+                    break # The dealer has busted.
+                input('Press Enter to continue...')
+                print('\n\n')
+
+        # Show the final hands:
+        displayHands(playerHand, dealerHand, True)
+
+        playerValue = getHandValue(playerHand)
+        dealerValue = getHandValue(dealerHand)
+        # Handle whether the player won, lost, or tied:
+        if dealerValue > 21:
+            print('Dealer busts! You win ${}!'.format(bet))
+            money += bet
+        elif (playerValue > 21) or (playerValue < dealerValue):
+            print('You lost!')
+            money -= bet
+        elif playerValue > dealerValue:
+            print('You won ${}!'.format(bet))
+            money += bet
+        elif playerValue == dealerValue:
+            print('It\'s a tie, the bet is returned to you.')
+        
+        input('Press Enter to continue...')
+        print('\n\n')
+
+
+def getBet(maxBet):
+    """Ask th eplayer how much they want to bet for this round."""
+    while True: # Keep asking until they enter a valid amount.
+        print('How much do you bet? (1-{}, or QUIT)'.format(maxBet))
+        bet = input('> ').upper().strip()
+        if bet == 'QUIT':
+            print('Thanks for playing!')
+            sys.exit()
